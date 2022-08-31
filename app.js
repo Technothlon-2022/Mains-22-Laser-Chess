e41429b7a4cbb03196601f6d0f579b88eae176d5
shankar <gowrishankarirwog@gmail.com>
2022-09-01 00:08:30 +0530

    extended socket

--- app.js
+++ app.js
@@ -37,49 +37,64 @@ const client = new MongoClient(MONGO_URI);
 
 //******************Database connection***************//
 
-// const uri = "mongodb+srv://technoUser:YLKHH2jNdicMSrJB@cluster3.3ahhuxr.mongodb.net/Techno_Database";
-// const { roll } = require('ejs');
-const users = {};
-// const client = new MongoClient(uri);
+// const { response } = require("express");
+ const users = {};
 
-io.on("connection", (socket) => {
-	socket.on("new-user-joined", async (roll) => {
-		console.log("New user: ", roll);
-		// console.log("User Connected: "+ socket.id);
-		users[socket.id] = roll;
-		const room = await findOneListingByRoll(client, roll);
-		console.log("Room: ", room);
-		socket.join(room);
-		roomSize = io.sockets.adapter.rooms.get(room).size;
-		console.log(room + ":" + roomSize);
-		io.to(room).emit("user-joined", roll);
-		socket.on("message", (data) => {
-			console.log(users[socket.id] + ": " + data);
-			io.to(room).emit("message", data);
-			updateListingByRoom(client, room, { message: data });
-		});
-	});
+io.on("connection",(socket)=>{
+    socket.on('new-user-joined',async (roll) =>{
+                console.log("New user: ",roll);
+                // console.log("User Connected: "+ socket.id);
+               users[socket.id]=roll;
+                const room= await findOneListingByRoll(client,roll)
+                console.log("Room: ",room);
+                socket.join(room);
+                roomSize=io.sockets.adapter.rooms.get(room).size
+                console.log(room+":"+roomSize);
+                io.to(room).emit('user-joined',roll);
+                socket.on("message",(data)=>{
+                    console.log(users[socket.id]+": "+data);
+                    io.to(room).emit('message',data)
+                    updateListingByRoom(client,room, {message:data});
+                });
+                socket.on("score",(data)=>{
+                    console.log(data);
+                    // io.to(room).emit('message',data)
+                    updateListingByRoll(client,users[socket.id], {score:data});
+                });
+                socket.on("move",(data)=>{
+                    console.log(data);
+                    socket.to(room).emit('move',data)
+                    updateListingByRoll(client,users[socket.id], {move:data});
+                });
+                socket.on("board",(data)=>{
+                     console.log(data);
+                    updateListingByRoom(client,room, {board:data});
+                });
+            });   
 });
 async function updateListingByRoom(client, roomName, updatedListing) {
-	const result = await client
-		.db("Techno_Database")
-		.collection("users")
-		.updateMany({ room: roomName }, { $set: updatedListing });
-	// console.log(`${result.matchedCount} document(s) matched the query criteria.`);
-	console.log(`${result.modifiedCount} document(s) was/were updated.`);
+    const result = await client.db("Techno_Database").collection("users").updateMany({ room:roomName }, { $set: updatedListing });
+    // console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+async function updateListingByRoll(client, rollno, updatedListing) {
+    const result = await client.db("Techno_Database").collection("users").updateOne({ roll:rollno }, { $set: updatedListing });
+    // console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
 }
 
 async function findOneListingByRoll(client, roll) {
-	const result = await client.db("Techno_Database").collection("users").findOne({ roll: roll });
-	const response = result.room;
-	// console.log(response);
-	if (result) {
-		console.log(`Found a listing in the collection with the roll '${roll}':`);
-		// console.log(result.room);
-	} else {
-		console.log(`No listings found with the roll '${roll}'`);
-	}
-	return response;
+    const result = await client.db("Techno_Database").collection("users").findOne({ roll: roll })
+    const response=result.room
+    // console.log(response);
+       if (result) {
+        console.log(`Found a listing in the collection with the roll '${roll}':`);
+        // console.log(result.room);
+    } else {
+        console.log(`No listings found with the roll '${roll}'`);
+    }
+    return response
+    
 }
 
 //*******End of Database part ***********//
